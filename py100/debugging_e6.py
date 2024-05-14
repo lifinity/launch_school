4e87e9176f627359b273cefe6a0cfb49ac122b07
lifinity <ll694@cornell.edu>
2024-05-14 01:53:17 -0400

    py100 debugging exercises

---
+++ py100/debugging_e6.py
@@ -0,0 +1,7 @@
+# populate list
+numbers = []
+
+for i in range(1, 6):
+    numbers.append(i)
+
+print(numbers)
---
+++ py100/debugging_e8.py
@@ -0,0 +1,10 @@
+# matrix
+sub_list = ["-", "-", "-"]
+matrix = []
+
+for _ in range(3):
+    matrix.append(list(sub_list))
+    # matrix.append(sub_list.copy())
+
+matrix[0][0] = "X"
+print(matrix) # [['X', '-', '-'], ['X', '-', '-'], ['X', '-', '-']]
