55b8a306c7df68f30839e743914324a2fb2223e6
lifinity <ll694@cornell.edu>
2024-05-14 01:43:01 -0400

    intro_python loops & iterating exercises

---
+++ intro_python/loops_e1.py
@@ -0,0 +1,6 @@
+counter = 0
+
+while counter < 5:
+    print(counter)
+
+# infinite loop because counter's never updated. always 0 & therefore < 5 -> returns truthy
---
+++ intro_python/loops_e3.py
@@ -0,0 +1,11 @@
+my_list = [6, 3, 0, 11, 20, 4, 17]
+
+print('WHILE LOOP:')
+idx = 0
+while idx < len(my_list):
+    print(my_list[idx])
+    idx += 1
+
+print('FOR LOOP:')
+for element in my_list:
+    print(element)
