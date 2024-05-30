79ef86f46392e7d2418354213a785f18e425a117
lifinity <ll694@cornell.edu>
2024-05-30 01:56:32 -0400

    + loan recap msg, handle nan case, screenclear btwn calcs & fixed typos

--- py101/loan_calculator.py
+++ py101/loan_calculator.py
@@ -1,8 +1,6 @@
 import json
-
-# load in external prompt & error msgs from JSON
-with open('loan_calculator_messages.json', 'r') as file:
-    MESSAGES = json.load(file)
+from os import system
+from math import isnan
 
 # constant definitions
 MONTHS_IN_YEAR = 12
@@ -11,14 +9,19 @@ MAX_APR = 100
 MIN_DURATION = 1
 MAX_DURATION = 100
 
+# load in external prompt/error msgs from JSON
+with open('loan_calculator_messages.json', 'r') as file:
+    MESSAGES = json.load(file)
+
 # function definitions
 def display_prompt(msg_key, extra_msg=''):
-    print(f'==> {MESSAGES[msg_key]} {extra_msg}')
+    msg = MESSAGES[msg_key] if msg_key else ''
+    print(f'==> {msg}{extra_msg}')
 
 def valid_number(num_str, num_ceiling, num_floor=0, num_type='float'):
     try:
         num = int(num_str) if num_type == 'int' else float(num_str)
-        if num > num_ceiling or num < num_floor:
+        if isnan(num) or num > num_ceiling or num < num_floor:
             raise ValueError
     except ValueError:
         return False
@@ -60,25 +63,28 @@ def get_continue():
 
 # main program definition
 def loan_calculator():
-    display_prompt('welcome')
-    print("------------------------------",
-          "------------------------------")
-
     while True:
+        # display program intro; clear first for consistent display
+        system('clear')
+        display_prompt('welcome')
+        print("------------------------------",
+              "------------------------------", sep='')
+
         # get loan amount from user
         loan_amount = get_loan_amount()
 
         if loan_amount > 0:
             # get annual percentage rate from user
-            annual_percentage = get_annual_percent() / 100
-            monthly_percentage = annual_percentage / MONTHS_IN_YEAR
+            annual_percent = get_annual_percent()
+            monthly_percent = (annual_percent / 100) / MONTHS_IN_YEAR
 
             # get loan duration rate from user
             loan_duration_months = get_loan_duration()
 
-            if monthly_percentage > 0:
-                loan_payment = loan_amount * (monthly_percentage / \
-                (1 - (1 + monthly_percentage)**(-loan_duration_months)))
+            if monthly_percent > 0:
+                # calculate monthly payment w/ provided inputs
+                loan_payment = loan_amount * (monthly_percent / \
+                (1 - (1 + monthly_percent)**(-loan_duration_months)))
             else:
                 # handle 0-interest edge case
                 loan_payment = loan_amount/loan_duration_months
@@ -86,23 +92,29 @@ def loan_calculator():
             # handle 0-loan amount edge case;
             loan_payment = 0
 
+        # display input recap & calculation results
+        display_prompt(None, f'${loan_amount:.2f} @ {annual_percent:.2f}% ' \
+                       + f'APR for {loan_duration_months} months.')
         display_prompt('loan_payment', f'${loan_payment:.2f}')
-        # ask user if they want to keep calculating?
+
+        # ask user if they want to run another calculation
         if not get_continue():
             break
 
-# run main program body
+# run main program
 if __name__ == "__main__":
     loan_calculator()
 
+# PERSONAL PROJECT NOTES
+# -----------------------------------------
 # user input:
 # loan amount (float; int ok too)
-# annual percentage rate (apr) (float; but unprocessed, i.e. 5 & not .05)
-# loan duration (months)
+# annual percentage rate (apr) (pre-processed float, i.e. 5 & not .05)
+# loan duration (int, months only)
 
 # calculate:
 # monthly interest rate (apr/100)/12
-# loan duration in months
+# loan duration in months (no calc; input already in months)
 
 # loan_payment = loan_amount * (monthly_interest_rate /
 # (1 - (1 + monthly_interest_rate)**loan_duration_months))
